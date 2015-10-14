fa5cc0f730bd47b54880254f6c00aca0ca704a18
PZ <pzika@langmaster.cz>
2015-10-14 21:16:04 +0200

    v

--- Web4/Migrations/201509010756168_v003.cs
+++ Web4/Migrations/201509010756168_v003.cs
@@ -1,24 +1,20 @@
-namespace blendedData
-{
-    using System;
-    using System.Data.Entity.Migrations;
-    
-    public partial class v003 : DbMigration
-    {
-        public override void Up()
-        {
-            DropIndex("dbo.CourseDatas", new[] { "TaskId" });
-            AlterColumn("dbo.CourseDatas", "TaskId", c => c.String(maxLength: 32));
-            AlterColumn("dbo.CourseDatas", "Data", c => c.String());
-            CreateIndex("dbo.CourseDatas", "TaskId");
-        }
-        
-        public override void Down()
-        {
-            DropIndex("dbo.CourseDatas", new[] { "TaskId" });
-            AlterColumn("dbo.CourseDatas", "Data", c => c.String(nullable: false));
-            AlterColumn("dbo.CourseDatas", "TaskId", c => c.String(nullable: false, maxLength: 240));
-            CreateIndex("dbo.CourseDatas", "TaskId");
-        }
+namespace blendedData {
+  using System;
+  using System.Data.Entity.Migrations;
+
+  public partial class v003 : DbMigration {
+    public override void Up() {
+      DropIndex("dbo.CourseDatas", new[] { "TaskId" });
+      AlterColumn("dbo.CourseDatas", "TaskId", c => c.String(maxLength: 32));
+      AlterColumn("dbo.CourseDatas", "Data", c => c.String());
+      CreateIndex("dbo.CourseDatas", "TaskId");
     }
+
+    public override void Down() {
+      DropIndex("dbo.CourseDatas", new[] { "TaskId" });
+      AlterColumn("dbo.CourseDatas", "Data", c => c.String(nullable: false));
+      AlterColumn("dbo.CourseDatas", "TaskId", c => c.String(nullable: false, maxLength: 240));
+      CreateIndex("dbo.CourseDatas", "TaskId");
+    }
+  }
 }
