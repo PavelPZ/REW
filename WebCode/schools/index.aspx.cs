72e71e21b17e5fcf49b954c851ae02e475a8d3d3
PZ <pzika@langmaster.cz>
2015-10-22 15:30:05 +0200

    cachovani index.aspx

--- WebCode/schools/index.aspx.cs
+++ WebCode/schools/index.aspx.cs
@@ -1,18 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace WebCode {
+
   public partial class index : System.Web.UI.Page {
-    protected void Page_Load(object sender, EventArgs e) {
+    protected void Page_Init(object sender, EventArgs e) {
+      if (isCached()) {
+        Response.StatusCode = 304;
+        Response.SuppressContent = true;
+        Response.End();
+      } else {
+        Response.Cache.SetLastModified(appLoadTime);
+      }
+
       cfg = new Packager.Config() {
         target = LMComLib.Targets.web,
-        version = schools.versions.debug,
-        //version = schools.versions.minified,
+        //version = schools.versions.debug,
+        version = schools.versions.minified,
         dataBatchUrl = "/lm/lm_data_new/",
         lang = LMComLib.urlInfo.langStrToLang(Request["lang"]),
         canSkipCourse = true,
@@ -23,6 +27,9 @@ namespace WebCode {
       };
       pageTitle = cfg.designId == "skrivanek" ? "Skřivánek" : "LANGMaster";
     }
+
+    static DateTime appLoadTime = DateTime.Now;
+
     protected Packager.Config cfg;
     protected string pageTitle;
     protected string scripts() {
@@ -37,9 +44,19 @@ namespace WebCode {
         sb.AppendLine("  <script src='../jslib/scripts/jquery.min.js' type='text/javascript'></script>");
         sb.AppendLine("  <script src='../deploy/externals.min.js' type='text/javascript'></script>");
         sb.AppendLine("  <script src='../deploy/web.min.js' type='text/javascript'></script>");
-        sb.AppendLine("  <script src='../deploy/"+ cfg.langStr  + ".min.js' type='text/javascript'></script>");
+        sb.AppendLine("  <script src='../deploy/" + cfg.langStr + ".min.js' type='text/javascript'></script>");
       }
       return sb.ToString();
+    }
+
+    bool isCached() { 
+      string header = Request.Headers["If-Modified-Since"]; //udaje, nastavene vyse v Response.Cache.SetLastModified(appLoadTime);
+      if (header != null) {
+        DateTime clientLastModified;
+        if (DateTime.TryParse(header, out clientLastModified))
+          return clientLastModified > appLoadTime.AddSeconds(-1); //musi se vyhladit nepresnost porovnani datumu
+      }
+      return false;
     }
   }
 
