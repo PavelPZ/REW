e0d19a2a7067fede6ce171bb2764b716e950aa43
PZ <pzika@langmaster.cz>
2015-10-23 11:02:07 +0200

    odstraneni nepotrebnych projektu

--- Blended/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Blended.Global" Language="C#" %>
--- Blended/app/config.js
+++
@@ -1,8 +0,0 @@
-﻿var config = {
-  runExMask: {
-    'english': 'http://localhost/Web4/Schools/NewEA.aspx?lang=cs-cz&persistType=persistMemory&displayMode=previewEx#/pg/old/school/schoolexmodel/1/@lm@blended@english@dialogs.product@//',
-    'german': 'http://localhost/Web4/Schools/NewEA.aspx?lang=cs-cz&persistType=persistMemory&displayMode=previewEx#/pg/old/school/schoolexmodel/1/@lm@blended@german@dialogs.product@//',
-    'french': 'http://localhost/Web4/Schools/NewEA.aspx?lang=cs-cz&persistType=persistMemory&displayMode=previewEx#/pg/old/school/schoolexmodel/1/@lm@blended@french@dialogs.product@//',
-    'lessons': 'http://localhost/Web4/Schools/NewEA.aspx?lang=cs-cz&persistType=persistMemory&displayMode=previewEx#/pg/old/school/schoolexmodel/1/@lm@prods_lm_blended_english_lessons_lesson_a1_1@//',
-  }
-};
