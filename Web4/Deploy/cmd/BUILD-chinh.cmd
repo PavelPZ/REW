e9ffa25a922d37d7e6064e31ea471bc17d5fe9c6
PZ <pzika@langmaster.cz>
2015-11-03 15:06:36 +0100

    v

--- Web4/Deploy/cmd/BUILD-chinh.cmd
+++ Web4/Deploy/cmd/BUILD-chinh.cmd
@@ -2,6 +2,7 @@ del d:\LMCom\rew\Web4\Data\buildLogs\*.* /q
 
 d:\LMCom\rew\CubesDeployment\bin\Debug\CubesDeployment.exe -9lmtests
 d:\LMCom\rew\CubesDeployment\bin\Debug\CubesDeployment.exe -9skrivanek
+d:\LMCom\rew\CubesDeployment\bin\Debug\CubesDeployment.exe -9edusoft
 d:\LMCom\rew\CubesDeployment\bin\Debug\CubesDeployment.exe -9globals-chinh
 
 call d:\LMCom\rew\Web4\Deploy\cmd\copyFE3.cmd
