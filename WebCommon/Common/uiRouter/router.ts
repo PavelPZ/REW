56063e52e8545d21326cd0dbc3ec945248623fc5
PZ <pzika@langmaster.cz>
2015-11-09 15:03:40 +0100

    WebCommon, router finish, flux start

---
+++ WebCommon/Common/uiRouter/router.ts
@@ -0,0 +1,20 @@
+﻿declare namespace angular {
+  class UrlMatcher {
+    constructor(pattern: string);
+    exec(url: string, query?: {}): {};
+    format(params: {}): string;
+  }
+  class $UrlMatcherFactory {
+  }
+}
+namespace angular {
+  new angular.$UrlMatcherFactory();
+  export function testRouter() {
+    var urlMatcher = new angular.UrlMatcher('/user/:id/name/:name?opt1&opt2');
+    var pars = urlMatcher.exec('/useR/123/Name/alex', { opt1: 'xxx', opt2: 'yyy' });
+    var url = urlMatcher.format(pars);
+    pars = null;
+  }
+}
+
+
