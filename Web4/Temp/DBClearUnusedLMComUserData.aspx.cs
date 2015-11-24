bc5e19fd29fc5370d7c42b539e9413c01fc959a8
PZ <pzika@langmaster.cz>
2015-11-24 11:53:12 +0100

    Vyhozeni LMComDataModel.dbml

--- Web4/Temp/DBClearUnusedLMComUserData.aspx.cs
+++ Web4/Temp/DBClearUnusedLMComUserData.aspx.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using LMComLib;
-using LMNetLib;
-using System.IO;
-using System.Linq;
-using System.Data.Linq;
+﻿//using System;
+//using LMComLib;
+//using LMNetLib;
+//using System.IO;
+//using System.Linq;
+//using System.Data.Linq;
 
-namespace web4.Temp {
-  public partial class DBClearUnusedLMComUserData : System.Web.UI.Page {
-    protected void Page_Load(object sender, EventArgs e) {
+//namespace web4.Temp {
+//  public partial class DBClearUnusedLMComUserData : System.Web.UI.Page {
+//    protected void Page_Load(object sender, EventArgs e) {
 
-    }
-    protected void GoBtn_Click(object sender, EventArgs e) {
-      var ids = File.ReadAllLines(@"c:\Temp\lmcom.txt").Where(l => !string.IsNullOrEmpty(l)).Select(l => int.Parse(l)).ToArray();
-      var db = Machines.getContext();
-      db.CommandTimeout = 10000;
-      foreach (var interv in LowUtils.intervals(ids.Length, 500)) {
-        string inClausule = ids.Skip(interv.skip).Take(interv.take).Select(i => i.ToString()).Aggregate((r, i) => r + "," + i);
-        var res = db.ExecuteCommand("DELETE FROM [CourseUser] WHERE UserId in (" + inClausule + ")");
-        //if (res2 != interv.take) break;
-        //break;
-      }
-    }
-  }
-}
+//    }
+//    protected void GoBtn_Click(object sender, EventArgs e) {
+//      var ids = File.ReadAllLines(@"c:\Temp\lmcom.txt").Where(l => !string.IsNullOrEmpty(l)).Select(l => int.Parse(l)).ToArray();
+//      var db = Machines.getContext();
+//      db.CommandTimeout = 10000;
+//      foreach (var interv in LowUtils.intervals(ids.Length, 500)) {
+//        string inClausule = ids.Skip(interv.skip).Take(interv.take).Select(i => i.ToString()).Aggregate((r, i) => r + "," + i);
+//        var res = db.ExecuteCommand("DELETE FROM [CourseUser] WHERE UserId in (" + inClausule + ")");
+//        //if (res2 != interv.take) break;
+//        //break;
+//      }
+//    }
+//  }
+//}
