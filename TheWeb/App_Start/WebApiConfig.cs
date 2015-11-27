cdf8e26650b7dee4a5313c02651115a305cb0dd5
PZ <pzika@langmaster.cz>
2015-11-27 16:27:55 +0100

    v

--- TheWeb/App_Start/WebApiConfig.cs
+++ TheWeb/App_Start/WebApiConfig.cs
@@ -13,8 +13,7 @@ namespace TheWeb {
        
       config.Routes.MapHttpRoute(
           name: "DefaultApi",
-          routeTemplate: "api/{controller}/{id}",
-          defaults: new { id = RouteParameter.Optional }
+          routeTemplate: "api/{controller}/{action}"
       );
     }
   }
--- TheWeb/Services/System.cs
+++ TheWeb/Services/System.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LMNetLib;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -8,7 +10,20 @@ using System.Web.Http;
 namespace TheWeb.Services {
 
   public class SystemController : ApiController {
-    [HttpGet] // api/system/resetcache
+
+    [ActionName("resetcache"), HttpGet] // api/system/resetcache, http://stackoverflow.com/questions/31622795/webapi-error-when-post-method-is-created-multiple-actions-were-found-that-mat: If you want to use [ActionName] attributes on your controller actions, then your route template needs to include {action}
     public void ResetCache() { Cache.init(); }
+
+    [ActionName("deletelogfiles"), HttpGet] // api/system/deletelogfiles
+    public void DeleteLogFiles() {
+      LowUtils.TraceErrorCall("Services.SystemController.DeleteLogFiles", () => {
+        delDirContent(@"D:\home\LogFiles", false);
+        foreach (var dir in Directory.GetDirectories(@"D:\home\LogFiles")) delDirContent(dir, true);
+      });
+    }
+    void delDirContent(string dir, bool incDirs) {
+      //foreach (var fn in Directory.GetFiles(dir)) try { File.Delete(fn); } catch { }
+      if (incDirs) foreach (var subDir in Directory.GetDirectories(dir)) try { Directory.Delete(subDir, true); } catch { }
+    }
   }
 }
